8807901a625715a1c7d36f29455edd41a3a6d26c
Jose Muniz <jmuniz900@gmail.com>
2023-02-10 17:53:35 -0800

    discovered timer. implemented basics

--- main.cpp
+++ main.cpp
@@ -1,12 +1,38 @@
 #include "hangman.h"
 #include "words.h"
 #include <iostream>
+#include <time.h>
 
 using namespace std;
 
-int main(){
-    WordData item;
-    cout << item.chooseWord();
-    return 0;
+void start(){
+    clock_t timer;
+    float seconds;
 
+    timer = clock();
+    timer = clock() - timer;
+    seconds = ((float)timer)/CLOCKS_PER_SEC;
+
+}
+
+int main(){
+    int choice;
+
+    cout << "   Main Menu" << endl;
+    cout << "---------------" << endl;
+    cout << "1. Start Game" << endl;
+    cout << "2. " << endl;
+
+
+    switch (choice)
+    {
+    case 1:
+        start();
+        break;
+    case 2:
+
+    
+    default:
+        break;
+    }
 }
